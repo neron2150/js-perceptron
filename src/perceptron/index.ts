c01e99ef54d00edc3fd53de59ad91d4e62251afc
Nikita Abrazhevich <neron2150@gmail.com>
2023-02-19 16:36:23 +0100

    Added basic structure. Added solid js view layer. Removed unnecessary files from solid js template.

---
+++ src/perceptron/index.ts
@@ -0,0 +1,1 @@
+export { Perceptron } from "./perceptron";
---
+++ src/view/App.tsx
@@ -0,0 +1,13 @@
+import type { Component } from 'solid-js';
+
+import styles from './App.module.css';
+
+const App: Component = () => {
+  return (
+    <div >
+
+    </div>
+  );
+};
+
+export default App;
