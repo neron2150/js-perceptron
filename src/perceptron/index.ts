b252a60d8e1d05c78eebfdf269d893ca8d4ccc99
Nikita Abrazhevich <neron2150@gmail.com>
2023-09-27 16:40:09 +0200

    change to react + vite

--- src/perceptron/index.ts
+++
@@ -1,1 +0,0 @@
-export { Perceptron } from "./perceptron";
--- src/view/index.tsx
+++
@@ -1,9 +0,0 @@
-
-import { render } from 'solid-js/web';
-
-import './index.css';
-import App from './App';
-
-const root = document.getElementById('root');
-
-render(() => <App />, root!);
